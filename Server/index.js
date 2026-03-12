b300aa3c293022bb7577236609833b7767bd9530
onaolapomay <raphy.mayo@gmail.com>
2026-03-12 11:20:49 +1100

    enable global cors preflight

--- Server/index.js
+++ Server/index.js
@@ -15,12 +15,8 @@ app.use(cors({
   origin: [ 'http://localhost:5173',
     'https://expensesrack.netlify.app'
   ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
 }))
 
-app.options('/api/auth/login', cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
