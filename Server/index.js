d2aa2e682b8a165d2d6b368c864262ce9cfb3296
onaolapomay <raphy.mayo@gmail.com>
2026-03-13 22:37:12 +1100

    update cors configuration

--- Server/index.js
+++ Server/index.js
@@ -15,6 +15,10 @@ app.use(cors({
   origin: [ 'http://localhost:5173',
     'https://expensesrack.netlify.app'
   ],
+
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 }))
 
 
