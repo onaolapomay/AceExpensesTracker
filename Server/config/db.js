d7c08f477f62b24dc02954d942dbdfc530eb53fa
onaolapomay <raphy.mayo@gmail.com>
2026-06-14 00:14:00 +0100

    clean up MongoDB connection logging

--- Server/config/db.js
+++ Server/config/db.js
@@ -5,9 +5,10 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI)
         console.log('MongoDB connected')
     } catch (error) {
-        console.error('MongoDB connection faiied:', error.message)
-        process.exit(1)
-    }
+    console.error('MongoDB connection failed:')
+    console.error(error)
+    process.exit(1)
+}
 }
 
 module.exports = connectDB
